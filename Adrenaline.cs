9bb21f71fb3804579c83c7d03379dc600236679a
Andrinall <adr.wloos@gmail.com>
2024-08-02 21:53:30 +0300

    add project files

---
+++ Adrenaline.cs
@@ -0,0 +1,26 @@
+﻿using MSCLoader;
+
+namespace Adrenaline
+{
+    public class Adrenaline : Mod
+    {
+        public override string ID => "com.adrenaline.mod";
+        public override string Name => "Adrenaline";
+        public override string Author => "Andrinall,@racer";
+        public override string Version => "0.0.1";
+        public override string Description => "";
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+        }
+
+        private void Mod_OnLoad()
+        {
+            AdrenalineBar.Init();
+            AdrenalineBar.Set(72);
+
+            ModConsole.Print("[Adrenaline]: <color=green>Successfully loaded!</color>");
+        }
+    }
+}
---
+++ AdrenalineBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Adrenaline
+{
+    internal class AdrenalineBar
+    {
+        internal static Material Mat;
+        internal static Transform HUD;
+        internal static Transform Bar;
+
+        public static void Init()
+        {
+            HUD = GameObject.Find("GUI/HUD").transform;
+            HUD.Find("Money").transform.localPosition = new Vector3(-11.5f, 6.4f);
+            Transform clone = Object.Instantiate(HUD.Find("Stress"));
+            Transform label = clone.Find("HUDLabel");
+            clone.name = "Adrenaline";
+            clone.parent = HUD;
+            label.GetComponent<TextMesh>().text = "Adrenaline";
+            label.Find("HUDLabelShadow").GetComponent<TextMesh>().text = "Adrenaline";
+            clone.localPosition = new Vector3(-11.5f, 6.8f);
+            Object.Destroy(clone.GetComponentInChildren<PlayMakerFSM>());
+
+            Bar = GameObject.Find("GUI/HUD/Adrenaline/Pivot").transform;
+            Mat = Bar.Find("HUDBar").GetComponent<MeshRenderer>().material;
+
+            Mat.color = Color.white;
+        }
+
+        public static void Set(int value)
+        {
+            Bar.localScale = new Vector3(Mathf.Clamp(value / 100f, 0f, 1f), 1f);
+        }
+    }
+}
