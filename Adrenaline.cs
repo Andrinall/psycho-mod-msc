691066ec5a5645c31e5bf1833fc5acf6222f8a9d
Andrinall <adr.wloos@gmail.com>
2024-08-27 11:23:29 +0300

    bump version

--- Adrenaline.cs
+++ Adrenaline.cs
@@ -13,7 +13,7 @@ namespace Adrenaline
         public override string ID => "com.adrenaline.mod";
         public override string Name => "Adrenaline";
         public override string Author => "Andrinall,@racer";
-        public override string Version => "0.0.1";
+        public override string Version => "0.15.9";
         public override string Description => "";
 
         internal readonly List<CarData> CARS = new List<CarData> {
