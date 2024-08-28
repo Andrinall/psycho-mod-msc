ade7437cc7415fcafc1d3be49e6dc133182e33bc
Andrinall <adr.wloos@gmail.com>
2024-08-28 08:41:13 +0300

    fix null reference exception into HighSpeedHandler component

--- HighSpeedHandler.cs
+++ HighSpeedHandler.cs
@@ -6,7 +6,6 @@ namespace Adrenaline
     internal class HighSpeedHandler : MonoBehaviour
     {
         public string CarName;
-        private float RequiredSpeed;
         private Drivetrain drivetrain;
         private FsmString playerVehicle;
 
@@ -14,17 +13,18 @@ namespace Adrenaline
         {
             drivetrain = base.GetComponent<Drivetrain>();
             playerVehicle = Utils.GetGlobalVariable<FsmString>("PlayerCurrentVehicle");
-            RequiredSpeed = (float)typeof(Configuration).GetField("REQUIRED_SPEED_" + CarName).GetValue(AdrenalineLogic.config);
             Utils.PrintDebug("HighSpeedHandler enabled");
         }
 
         private void FixedUpdate()
         {
             if (playerVehicle.Value != CarName) return;
+
+            var RequiredSpeed = (float)typeof(Configuration).GetField("REQUIRED_SPEED_" + CarName).GetValue(AdrenalineLogic.config);
             if (drivetrain.differentialSpeed > RequiredSpeed)
             {
                 AdrenalineLogic.IncreaseTimed(AdrenalineLogic.config.HIGHSPEED_INCREASE);
-                Utils.PrintDebug("Value increased by driving on high speed");
+                Utils.PrintDebug("Value++ by driving on high speed on " + CarName);
             }
         }
     }
