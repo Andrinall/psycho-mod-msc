f5ff2e1004e4624fc65c250a47999f0708cfec45
Andrinall <adr.wloos@gmail.com>
2024-08-28 15:14:30 +0300

    change SpillHandler behaviour

--- SpillHandler.cs
+++ SpillHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using HutongGames.PlayMaker;
+using System.Linq;
 using UnityEngine;
 
 namespace Adrenaline
@@ -6,16 +7,18 @@ namespace Adrenaline
     internal class SpillHandler : MonoBehaviour
     {
         private PlayMakerFSM SpillPump;
+        private FsmBool IsCrime;
 
         private void OnEnable()
         {
             SpillPump = base.GetComponents<PlayMakerFSM>().FirstOrDefault(v => v.FsmName == "SpillPump");
+            IsCrime = SpillPump.FsmVariables.GetFsmBool("Crime");
             Utils.PrintDebug("SpillHandler enabled");
         }
 
         private void FixedUpdate()
         {
-            if (SpillPump?.ActiveStateName == "Spill grow")
+            if (SpillPump?.ActiveStateName == "Spill grow" && IsCrime?.Value == true)
             {
                 AdrenalineLogic.IncreaseTimed(AdrenalineLogic.config.SPILL_SHIT);
                 Utils.PrintDebug("Value increased by spill grow");
