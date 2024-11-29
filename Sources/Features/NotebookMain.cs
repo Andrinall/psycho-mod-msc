026827ef773bc957babf856012ebd49e243caf0b
Andrinall <adr.wloos@gmail.com>
2024-11-29 16:29:05 +0500

    skelet for notebook system

---
+++ Sources/Features/NotebookMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Psycho.Sources.Features
+{
+    internal class NotebookMain : MonoBehaviour
+    {
+        void Awake()
+        {
+
+        }
+
+        public int CalcTruePages()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Sources/Features/NotebookPage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Psycho.Sources.Features
+{
+    internal class NotebookPage : MonoBehaviour
+    {
+        public int currentPageIndex;
+        public bool isDefaultPage;
+        public bool isFinalPage;
+        public bool isTruePage;
+
+        TextMesh textMesh;
+        NotebookMain Notebook;
+
+        void Awake()
+        {
+            Notebook = GameObject.Find("").GetComponent<NotebookMain>();
+        }
+
+        public void UpdatePageText()
+        {
+            int lang = Globals.CurrentLang;
+
+            if (isDefaultPage)
+            {
+                textMesh.text = Locales.DEFAULT_PAGE[lang];
+                return;
+            }
+
+            if (isFinalPage)
+            {
+                bool isTrueFinal = Notebook.CalcTruePages() > 7;
+                textMesh.text = Locales.FINAL_PAGE[isTrueFinal ? 0 : 1, lang];
+                return;
+            }
+
+            textMesh.text = Locales.PAGES[currentPageIndex, isTruePage ? 0 : 1, Globals.CurrentLang];
+        }
+    }
+}
