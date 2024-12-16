8de6ece3717adfc9fa311f1e782d2bf197a9b86a
Andrinall <adr.wloos@gmail.com>
2024-12-16 20:59:27 +0500

    StateHook log output improvement

---
+++ Sources/Internal/FsmHookAction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using MSCLoader;
+using HutongGames.PlayMaker;
+
+using Psycho.Extensions;
+
+
+namespace Psycho.Internal
+{
+    internal sealed class FsmHookActionWithArg : FsmStateAction
+    {
+        public Action<PlayMakerFSM> hook;
+        public PlayMakerFSM component;
+        public string method;
+        public string state;
+        public int index;
+
+
+        public override void OnEnter()
+        {
+            try
+            {
+                hook?.Invoke(component);
+            }
+            catch (Exception e)
+            {
+                ModConsole.Error(
+$@"Error in StateHook action delegate!
+[{component.transform.GetPath()}]
+[fsm {component.Fsm.Name}][state {state}][index {index}]
+[method {method}]"
+                );
+                ModConsole.Error(e.GetFullMessage());
+            }
+
+            base.Finish();
+        }
+    }
+
+    internal sealed class FsmHookAction : FsmStateAction
+    {
+        public Action hook;
+        public PlayMakerFSM component;
+        public string method;
+        public string state;
+        public int index;
+
+
+        public override void OnEnter()
+        {
+            try
+            {
+                hook?.Invoke();
+            }
+            catch (Exception e)
+            {
+                ModConsole.Error(
+$@"Error in StateHook action delegate!
+[{component.transform.GetPath()}]
+[fsm {component.Fsm.Name}][state {state}][index {index}]
+[method {method}]"
+                );
+                ModConsole.Error(e.GetFullMessage());
+            }
+
+            base.Finish();
+        }
+    }
+}
--- Sources/Internal/StateHook.cs
+++ Sources/Internal/StateHook.cs
@@ -5,34 +5,13 @@ using MSCLoader;
 using UnityEngine;
 using HutongGames.PlayMaker;
 
+using Psycho.Extensions;
+
 
 namespace Psycho.Internal
 {
     internal sealed class StateHook : FsmHook
     {
-        internal sealed class FsmHookAction : FsmStateAction
-        {
-            public Action<PlayMakerFSM> hook;
-            public PlayMakerFSM component;
-            public string path;
-
-
-            public override void OnEnter()
-            {
-                try
-                {
-                    hook?.Invoke(component);
-                }
-                catch (Exception e)
-                {
-                    ModConsole.Error($"Error in StateHook action delegate: {path}");
-                    ModConsole.Error(e.GetFullMessage());
-                }
-
-                base.Finish();
-            }
-        }
-
         public static void Inject(GameObject gameObject, string stateName, Action hook)
         {
             try
@@ -45,33 +24,7 @@ namespace Psycho.Internal
             }
         }
 
-        public static void Inject(GameObject gameObject, string fsmName, string stateName, Action<PlayMakerFSM> hook)
-        {
-            try
-            {
-                PlayMakerFSM playMaker = gameObject.GetPlayMaker(fsmName);
-                if (playMaker == null) return;
-
-                playMaker.Fsm.InitData();
-                FsmState playMakerState = gameObject.GetPlayMakerState(stateName);
-                if (playMakerState == null) return;
-
-                var list = new List<FsmStateAction>(playMakerState.Actions);
-                FsmHookAction item = new FsmHookAction {
-                    hook = hook,
-                    path = $"obj `{gameObject.name}` | fsm `{fsmName}` | state `{stateName}`",
-                    component = playMaker
-                };
-                list.Insert(0, item);
-                playMakerState.Actions = list.ToArray();
-            }
-            catch
-            {
-                ModConsole.Error($"FsmInject: Cannot find FSM <b>{fsmName}</b>, state <b>{stateName}</b> in GameObject <b>{gameObject.name}</b>");
-            }
-        }
-
-        public static void Inject(GameObject gameObject, string fsmName, string stateName, int index, Action<PlayMakerFSM> hook)
+        public static void Inject(GameObject gameObject, string fsmName, string stateName, Action hook, int index = 0)
         {
             try
             {
@@ -83,9 +36,12 @@ namespace Psycho.Internal
                 if (playMakerState == null) throw new NullReferenceException();
 
                 var list = new List<FsmStateAction>(playMakerState.Actions);
-                FsmHookAction item = new FsmHookAction {
+                FsmHookAction item = new FsmHookAction
+                {
                     hook = hook,
-                    path = $"obj `{gameObject.name}` | fsm `{fsmName}` | state `{stateName}` [{index}]",
+                    state = stateName,
+                    method = Utils.GetMethodPath(hook.Method),
+                    index = index,
                     component = playMaker
                 };
 
@@ -94,8 +50,48 @@ namespace Psycho.Internal
             }
             catch
             {
-                ModConsole.Error($"FsmInject: Cannot find FSM <b>{fsmName}</b>, state <b>{stateName}</b> in GameObject <b>{gameObject.name}</b>");
+                ModConsole.Error($"FsmInject: Cannot find FSM <b>{fsmName}</b>, state <b>{stateName}</b> in GameObject <b>'{gameObject.transform.GetPath()}'</b>");
             }
+        }
+
+        public static void Inject(GameObject gameObject, string fsmName, string stateName, Action<PlayMakerFSM> hook, int index = 0)
+        {
+            try
+            {
+                PlayMakerFSM playMaker = gameObject.GetPlayMaker(fsmName);
+                if (playMaker == null) throw new NullReferenceException();
+
+                playMaker.Fsm.InitData();
+                FsmState playMakerState = gameObject.GetPlayMakerState(stateName);
+                if (playMakerState == null) throw new NullReferenceException();
+
+                var list = new List<FsmStateAction>(playMakerState.Actions);
+                FsmHookActionWithArg item = new FsmHookActionWithArg
+                {
+                    hook = hook,
+                    state = stateName,
+                    method = Utils.GetMethodPath(hook.Method),
+                    index = index,
+                    component = playMaker
+                };
+
+                InsertItemWithIndex(index, ref list, ref item);
+                playMakerState.Actions = list.ToArray();
+            }
+            catch
+            {
+                ModConsole.Error($"FsmInject: Cannot find FSM <b>{fsmName}</b>, state <b>{stateName}</b> in GameObject <b>'{gameObject.transform.GetPath()}'</b>");
+            }
+        }
+
+        static void InsertItemWithIndex(int index, ref List<FsmStateAction> list, ref FsmHookActionWithArg item)
+        {
+            if (index > list.Count - 1 || index == -1)
+                list.Add(item);
+            else if (index < 0)
+                list.Insert(0, item);
+            else
+                list.Insert(index, item);
         }
 
         static void InsertItemWithIndex(int index, ref List<FsmStateAction> list, ref FsmHookAction item)
