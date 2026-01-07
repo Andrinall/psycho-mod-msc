b9223ee0723c1d057a53087a08b8989d2e50dac8
Andrinall <62207048+Andrinall@users.noreply.github.com>
2026-01-07 18:26:55 +0500

    Fix: invisible pills, walnut & mushroom.

--- Sources/Psycho.cs
+++ Sources/Psycho.cs
@@ -21,7 +21,7 @@ namespace Psycho
         public override string ID => "PsychoMod";
         public override string Name => "Psycho";
         public override string Author => "LUAR, Andrinall, @racer";
-        public override string Version => "1.0.13";
+        public override string Version => "1.0.14";
         public override string Description => "Adds a schizophrenia for your game character";
         
         public override byte[] Icon => Properties.Resources.mod_icon;
